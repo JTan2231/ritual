e99f66bc5700156e85aace62819df05c5b7798cf
joey <j.tan2231@gmail.com>
2024-03-11 19:44:21 -0500

    summaries

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type LogRequest struct {
@@ -23,6 +24,28 @@ type SummaryRequest struct {
 	ActivityName string `json:"activity_name"`
 	BeginDate    string `json:"begin_date"`
 	EndDate      string `json:"end_date"`
+}
+
+const API = "http://localhost:5000"
+
+func colorize(text, color string) string {
+	colors := map[string]string{
+		"reset":  "\033[0m",
+		"red":    "\033[31m",
+		"green":  "\033[32m",
+		"yellow": "\033[33m",
+		"blue":   "\033[34m",
+		"purple": "\033[35m",
+		"cyan":   "\033[36m",
+		"white":  "\033[1;37m",
+	}
+
+	colorCode, ok := colors[color]
+	if !ok {
+		colorCode = colors["reset"]
+	}
+
+	return colorCode + text + colors["reset"]
 }
 
 func log() {
@@ -54,7 +77,7 @@ func log() {
 		return
 	}
 
-	url := "http://localhost:5000/add-activity"
+	url := API + "/add-activity"
 	method := "POST"
 
 	client := &http.Client{}
@@ -91,6 +114,42 @@ func countToNumber(count string) int {
 		fmt.Println("countToNumber error:", err)
 		return 0
 	}
+}
+
+func formatText(title string, text string, lineLength int) string {
+	words := strings.Fields(text)
+	lines := []string{}
+	currentLine := ""
+
+	lines = append(lines, colorize(fmt.Sprintf("┃ %-*s ┃", lineLength, title), "yellow"))
+	lines = append(lines, colorize(fmt.Sprintf("┃ %-*s ┃", lineLength, ""), "yellow"))
+
+	for _, word := range words {
+		if len(currentLine)+len(word)+1 <= lineLength {
+			if currentLine != "" {
+				currentLine += " "
+			}
+			currentLine += word
+		} else {
+			lines = append(lines, currentLine)
+			currentLine = word
+		}
+	}
+
+	if currentLine != "" {
+		lines = append(lines, currentLine)
+	}
+
+	for i := 2; i < len(lines); i++ {
+		lines[i] = colorize("┃", "yellow") + colorize(fmt.Sprintf(" %-*s ", lineLength, lines[i]), "white") + colorize("┃", "yellow")
+	}
+
+	border := colorize("┏"+strings.Repeat("━", lineLength+2)+"┓", "yellow")
+	result := []string{border}
+	result = append(result, lines...)
+	result = append(result, colorize("┗"+strings.Repeat("━", lineLength+2)+"┛", "yellow"))
+
+	return strings.Join(result, "\n")
 }
 
 func summaryUsage() {
@@ -140,9 +199,9 @@ func summary(interval string) {
 			}
 		}
 
-		baseURL := "http://yourflaskappdomain.com/endpoint"
-		beginDate := "2023-01-01"
-		endDate := "2023-01-31"
+		baseURL := API + "/get-summary"
+		endDate := time.Now()
+		beginDate := endDate.AddDate(0, 0, -dayCount)
 
 		// Create URL with query parameters
 		u, err := url.Parse(baseURL)
@@ -152,8 +211,8 @@ func summary(interval string) {
 		}
 
 		queryParams := u.Query()
-		queryParams.Set("beginDate", beginDate)
-		queryParams.Set("endDate", endDate)
+		queryParams.Set("beginDate", beginDate.Format("2006-01-02"))
+		queryParams.Set("endDate", endDate.AddDate(0, 0, 1).Format("2006-01-02"))
 		u.RawQuery = queryParams.Encode()
 
 		resp, err := http.Get(u.String())
@@ -163,15 +222,22 @@ func summary(interval string) {
 		}
 		defer resp.Body.Close()
 
-		body, err := ioutil.ReadAll(resp.Body)
+		body, err := io.ReadAll(resp.Body)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
 
-		fmt.Printf("Response from Flask server: %s", string(body))
+		var jsonData map[string]interface{}
+		err = json.Unmarshal(body, &jsonData)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
 
-		fmt.Println("got", strconv.Itoa(dayCount), "days")
+		if val, ok := jsonData["response"]; ok {
+			fmt.Println(formatText(fmt.Sprintf("Summary of %s to %s", beginDate.Format("2006-01-02"), endDate.Format("2006-01-02")), val.(string), 80))
+		}
 	} else {
 		summaryUsage()
 	}
